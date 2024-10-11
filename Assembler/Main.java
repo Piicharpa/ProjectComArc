a43ccaa35e8f90e1c7740b65c9803ca983c06ca3
Poon Boonchoowit <poon15112@gmail.com>
2024-10-11 16:25:56 +0700

    #

--- Assembler/Main.java
+++ Assembler/Main.java
@@ -3,7 +3,6 @@ package Assembler;
 public class Main {
 
     public static void main(String[] args) {
-        // ตัวอย่างการใช้ AssemblerProcessor
         AssemblerProcessor processor = new AssemblerProcessor();
 
         // Input file path
@@ -12,8 +11,6 @@ public class Main {
         // Output file path
         String outputFilePath = "Assembler/File/output.txt";
 
-        // เรียกใช้ method process เพื่อประมวลผล assembly input จากไฟล์ inputFilePath
-        // และเขียนผลลัพธ์ลงไฟล์ outputFilePath
         processor.process(inputFilePath, outputFilePath);
     }
 }
