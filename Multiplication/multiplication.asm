5e16645604b1b97084f14b1a270fbe4dee0f0555
bbam <nunnapats1822@gmail.com>
2024-10-11 20:28:22 +0700

    mo multi again jaa

--- Multiplication/multiplication.asm
+++ Multiplication/multiplication.asm
@@ -1,17 +1,17 @@
-        lw      0   1   c       Load multiplicand (c) into $1
-        lw      0   2   p       Load multiplier (p) into $2
+        lw      0   1   mcand   Load multiplicand (mcand) into $1
+        lw      0   2   mplier  Load multiplier (mplier) into $2
         lw      0   3   pos0    Initialize result to 0 in $3
         lw      0   4   pos1    Load constant 1 into $4 for LSB check
         lw      0   5   pos0    Initialize loop counter or stack pointer
 
-loop    beq     2   0   done    If p == 0, jump to done (end)
-        add     6   2   4       $6 = p & 1 (check LSB of p)
-        beq     6   0   sadd    If LSB of p is 0, skip addition
+loop    beq     2   0   done    If mplier == 0, jump to done (end)
+        and     6   2   4       $6 = mplier & 1 (check LSB of mplier)
+        beq     6   0   sadd    If LSB of mplier is 0, skip addition
 
-        add     3   1   3       result = result + c 
+        add     3   1   3       result = result + mcand 
 
-sadd    add     1   1   1       mcand = mcand << 1
-        add     2   2   2       mplier = mplier >> 1 
+sadd    add     1   1   1       mcand = mcand << 1 (shift left by 1)
+        add     2   2   2       mplier = mplier - 1 (decrement multiplier)
         
         beq     0   0   loop    Jump back to loop to continue the calculation
 
@@ -22,4 +22,9 @@ pos0    .fill   0       Constant 0 for initialization
 pos1    .fill   1       Constant 1 for checking LSB
 mcand   .fill   32766   Multiplicand (32766)
 mplier  .fill   10383   Multiplier (10383)
-stack   .fill   0       Stack for status checking                       
+
+stack   .fill   0       Stack for status checking
+        .fill   0       Additional stack space
+        .fill   0       Additional stack space
+        .fill   0       Additional stack space
+        .fill   0       Additional stack space
